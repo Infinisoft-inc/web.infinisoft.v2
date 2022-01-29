89a665fcd5ba6d2e4a6780be0653a5631be8320f
Martin Ouimet <mouimet@infini-soft.com>
2022-01-29 14:35:03 -0500

    Project list

--- packages/web/src/features/Projects/projectListMock.tsx
+++ packages/web/src/features/Projects/projectListMock.tsx
@@ -9,9 +9,36 @@ const documentList: Document[] = [
 ]
 
 export const projectListMock: Project[] = [
-    { id: 0, title: 'Web Portal', subtitle: 'Petit Pont', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem dolor ac volutpat, purus ultrices enim, ut. Phasellus urna rhoncus pharetra tristique sed felis faucibus ultrices.', extra: <ProjectDocuments list={documentList} /> },
-    { id: 1, title: 'SaaS ERP', subtitle: 'Petit Pont', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem dolor ac volutpat, purus ultrices enim, ut. Phasellus urna rhoncus pharetra tristique sed felis faucibus ultrices.', },
-    { id: 2, title: 'Infinicode', subtitle: 'Infinisoft', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem dolor ac volutpat, purus ultrices enim, ut. Phasellus urna rhoncus pharetra tristique sed felis faucibus ultrices.', },
-    { id: 3, title: 'VCE', subtitle: 'BDC', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem dolor ac volutpat, purus ultrices enim, ut. Phasellus urna rhoncus pharetra tristique sed felis faucibus ultrices.', },
-    { id: 4, title: 'Jumbotron', subtitle: 'Jean Coutu', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem dolor ac volutpat, purus ultrices enim, ut. Phasellus urna rhoncus pharetra tristique sed felis faucibus ultrices.', },
+    {
+        id: 0,
+        title: 'Web Portal',
+        subtitle: 'Petit Pont',
+        description: 'Multi tenant system with a public progressive web app portal offering subscriptions to broad range community services. Stack is React 17, Typescript 4.2, Antd, React Router 6, Less, Serverless, AWS, Lambda, IaC, Amplify, GraphQL, DynamoDB. State of the art designed with serverless lambda functions, event driven and micro front-end.',
+        extra: <ProjectDocuments list={documentList}
+        />
+    },
+    {
+        id: 1,
+        title: 'SaaS ERP',
+        subtitle: 'Petit Pont',
+        description: 'Multi tenant ERP system with features like calendar, appointments, resources management, tasks, followup, dashboards, automatic business processes, notifications, advanced report builder/viewer, accounts, role based access security control. Stack is React 17, Typescript 4.2, Antd, React Router 5, Less, Serverless, AWS, Lambda, IaC, Amplify, GraphQL, DynamoDB. Serverless backend design, event driven and monolitic front-end.',
+    },
+    {
+        id: 2,
+        title: 'Infinicode',
+        subtitle: 'Infinisoft',
+        description: 'This project is to bring developers experience to the next level. We are tired of wasting almost all our time boilerplating. So here is a Visual Code extension featuring wysywig ide for new and existing project, automatic boilerplating based on industry best practices, dynamic runtime code injection to hook inside app and reverse runtime code back to original sources with modifications. Using advanced semmntic language interpretation using AST and Babel.',
+    },
+    {
+        id: 3,
+        title: 'VCE',
+        subtitle: 'BDC',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem dolor ac volutpat, purus ultrices enim, ut. Phasellus urna rhoncus pharetra tristique sed felis faucibus ultrices.',
+    },
+    {
+        id: 4,
+        title: 'Jumbotron',
+        subtitle: 'Jean Coutu',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem dolor ac volutpat, purus ultrices enim, ut. Phasellus urna rhoncus pharetra tristique sed felis faucibus ultrices.',
+    },
 ];
