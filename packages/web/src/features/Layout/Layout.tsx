4116f64116bf5085cd17fabd451a4ce7417d8b34
CayaCoco <86335453+CayaCoco@users.noreply.github.com>
2022-01-29 00:26:54 -0500

    gg

--- packages/web/src/features/Layout/Layout.tsx
+++ packages/web/src/features/Layout/Layout.tsx
@@ -3,35 +3,34 @@
  * Infinisoft Inc.
  * www.infini-soft.com
  */
-import loadable from '@loadable/component';
-import './styles.scss';
+import loadable from "@loadable/component"
+import { Outlet } from "react-router-dom"
+import "./styles.scss"
 
-const Header = loadable(() => import(/* webpackChunkName: 'Header' */ '../Header/Header'))
+const Header = loadable(
+	() => import(/* webpackChunkName: 'Header' */ "../Header/Header")
+)
 
 const Layout = () => {
-    return <div className='layout'>
-        <div className='video-container'>
-            <video
-                loop
-                preload="auto"
-                autoPlay
-                muted
-                className='video'
-            >
-                <source src="assets/hero-home.mp4" type="video/mp4" />
-            </video>
-        </div>
+	return (
+		<div className='layout'>
+			<div className='video-container'>
+				<video loop preload='auto' autoPlay muted className='video'>
+					<source src='assets/hero-home.mp4' type='video/mp4' />
+				</video>
+			</div>
 
-        <div className='toplayer'>
-            <div className='container'>
-                <Header />
+			<div className='toplayer'>
+				<div className='container'>
+					<Header />
 
-                {/* <main className='content'>
-                    <Outlet />
-                </main> */}
-            </div>
-        </div>
-    </div>
+					<main className='content'>
+						<Outlet />
+					</main>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Layout
