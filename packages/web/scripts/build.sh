29a713659e1ea95e227879009fa9a2a111f24441
Martin Ouimet <mouimet@infini-soft.com>
2022-04-13 08:14:34 -0400

    tst

---
+++ packages/web/scripts/build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | sed  's/ *//g')
+PACKAGE_NAME=$(cat package.json \
+  | grep name \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | sed  's/ *//g')
+
+TAGS=$(lerna changed)
+
+# CURRENT="web@0.1.10"
+
+echo $CURRENT
+echo $PACKAGE_VERSION
+echo $PACKAGE_NAME
+echo $TAGS
+echo $CHANGED
+
+if [[ "$TAGS" == *"$CURRENT"* ]]; then
+    echo "Package unchanged!"
+fi
